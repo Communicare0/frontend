32054647183d7470a5d253e23a9b06943caf3fca
박성준 <rabi31x@gmail.com>
2025-11-02 12:25:11 +0900

    for share not merge

---
+++ src/styles/styledMain.jsx
@@ -0,0 +1,13 @@
+import { styled, css } from "styled-components";
+
+
+export const Container = styled.div`
+background: #F3F4F6;
+display: flex;
+width: 1440px;
+height: 900px;
+padding: 0 1px;
+justify-content: center;
+align-items: center;
+position: relative;
+`;
