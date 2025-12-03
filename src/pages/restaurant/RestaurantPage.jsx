86967ec388d13795a612d849b6d75d1ac61eb084
박성준 <rabi31x@gmail.com>
2025-12-03 12:20:03 +0900

    restaurant branch push

---
+++ src/pages/restaurant/RestaurantPage.jsx
@@ -0,0 +1,13 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState, /*useRef*/ } from "react";
+import s from "@styles/modules/restaurant/restaurant.module.css";
+
+
+export default function RestaurantPage() {
+    return (
+        <div style={{ padding: "24px" }}>
+            <h1>식당</h1>
+            <p>식당 기능 구현</p>
+        </div>
+    );
+}
